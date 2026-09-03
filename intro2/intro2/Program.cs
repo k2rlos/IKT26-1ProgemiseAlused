bbf251459e7e9166e4b54e20e176bf73369f7657
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 11:49:11 +0300

    03.09.2026

---
+++ intro2/intro2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace intro2 //see on projekti nimetus
+{
+    internal class Program // see on klassi nimetus
+    {
+        //meetotit ei saa panna namespace'i, kuna see on klassi sees
+        static void Main(string[] args) //see on peamine meetod, mis käivitatakse programmi käivitamisel
+        {
+            Console.WriteLine("Hello, World!"); // see on konsooli väljund, mis prindib "Hello, World" ekraanile
+            //Kui tahad topelt rida teha, siis vajuta Ctrl + D
+            //kui tahad kustutada rida, siis vajuta ctrl + x
+        }
+        //Breakpointi saab panna koodirea vasakule poolele, kus on hall ruut. Kui programm jõuab
+        //selle koodirea juurde, peatub see ja võimaldab teil uurida muutujate väärtusi ja programmi olekut.
+    }
+}
