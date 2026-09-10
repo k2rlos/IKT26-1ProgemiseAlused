2a5bb435c616fa0381514e54317c148a1498ed38
k2rlos <karlosrajarva@gmail.com>
2026-09-10 10:52:13 +0300

    IfAndElseNumbers

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kasutaja sisestab oma vanuse
+            Console.Write("Sisesta oma vanus: ");
+            int vanus = Convert.ToInt32(Console.ReadLine());
+
+            // If-else tingimuslause
+            if (vanus >= 18)
+            {
+                Console.WriteLine("Oled täisealine!");
+            }
+            else
+            {
+                Console.WriteLine("Oled alaealine!");
+                //konsoolis sisestame numbri
+                //see number salvestatakse string andmetüüpi
+                //toimub kontroll, kas sisestatud väärtus on number
+                //kasutada if ja else lauseid
+            }
+        }
+    }
+}
