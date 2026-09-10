3ee2d9c1c68e105be5789df442469b0541e9f902
k2rlos <karlosrajarva@gmail.com>
2026-09-10 10:20:49 +0300

    IfAndElse

---
+++ 10.09.26/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IfAndElse
+{
+    //Projekt nimetusega IfAndElse, mille sees asub class nimega Program. See klass sisaldab main meetodit, mis
+    //on programmi sisenemispunkt
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            //Muutuja nimega name, kuhu salvestatakse kasutaja sisestatud tekst
+            string name = Console.ReadLine();
+
+            //! tähendab "ei ole" ja == tähendab "on võrdne"
+            if (name != "")
+            {
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Sisestasid enda nime");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine("ERROR.nime ei sisestanud");
+                // Kui kasutaja ei sisesta midagi, siis kostub 2 piiksu
+                Console.Beep();
+                // ja oodatakse 1 sekund enne programmi lõppu
+                Thread.Sleep(1000);
+                Console.Beep();
+            }
+        }
+    }
+}
