b008f174bf4590ffee16130f979bfca0709848aa
k2rlos <karlosrajarva@gmail.com>
2026-09-10 11:41:47 +0300

    IfAndElseNesting2

---
+++ IfAndElseNesting2/IfAndElseNesting2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace IfAndElseNesting2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha üks if ja else nestimine iseseisvalt");
+            Console.WriteLine("teise else if-i sisse panna if ja else nestimine");
+            Console.WriteLine("sisesta oma vanus");
+            int vanus = Convert.ToInt32(Console.ReadLine());
+
+            if (vanus >= 18)
+            {
+                Console.WriteLine("Oled täisealine");
+            }
+            else
+            {
+                Console.WriteLine("Oled alaealine");
+            }
+        }
+    }
+}
